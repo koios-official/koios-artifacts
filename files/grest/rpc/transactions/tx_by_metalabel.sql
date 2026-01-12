efe0878b759408a1288f00dba26b9c09eee1ee67
Ola Ahlman <ola.ahlman@gmail.com>
2026-01-12 23:42:51 +0100

    tx_by_metalabel fix Old pushed file seem to be a duplicate of tx_cbor.sql
    Needs docs update and RPC verified but at least the function now does what its supposed to.
    There might be some performance issues through postgrest call if I remember correctly.

--- files/grest/rpc/transactions/tx_by_metalabel.sql
+++ files/grest/rpc/transactions/tx_by_metalabel.sql
@@ -1,64 +1,41 @@
-CREATE OR REPLACE FUNCTION grest.tx_cbor(_tx_hashes text [])
+CREATE OR REPLACE FUNCTION grest.tx_by_metalabel(_label word64type, _after_block_height integer DEFAULT 0)
 RETURNS TABLE (
   tx_hash text,
   block_hash text,
   block_height word31type,
   epoch_no word31type,
   absolute_slot word63type,
-  tx_timestamp integer,
-  cbor text
+  tx_timestamp integer
 )
 LANGUAGE sql STABLE
 AS $$
+
+  WITH tx_id_min as (
+    SELECT MIN(id) as tx_id
+    FROM tx
+    WHERE block_id = (
+      SELECT block_id
+      FROM block
+        INNER JOIN tx ON tx.block_id = block.id
+      WHERE block.block_no >= _after_block_height
+      ORDER BY block_no
+      LIMIT 1
+    )
+  )
+
   SELECT
     ENCODE(tx.hash::bytea, 'hex'),
     ENCODE(block.hash, 'hex'),
     block.block_no,
     block.epoch_no,
     block.slot_no,
-    EXTRACT(EPOCH FROM block.time)::integer,
-    ENCODE(tx_cbor.bytes::bytea, 'hex')
-  FROM public.tx
+    EXTRACT(EPOCH FROM block.time)::integer
+  FROM tx_metadata tm
+    INNER JOIN tx_id_min tim ON tm.tx_id >= tim.tx_id
+    INNER JOIN tx ON tx.id = tm.tx_id
     INNER JOIN block ON block.id = tx.block_id
-    LEFT JOIN public.tx_cbor ON tx.id = tx_cbor.tx_id
-  WHERE tx.hash::bytea = ANY(
-      SELECT
-        DECODE(hashes, 'hex')
-      FROM
-        UNNEST(_tx_hashes) AS hashes
-    )
-  ORDER BY tx.id;
+  WHERE tm.key = _label;
+
 $$;
 
-COMMENT ON FUNCTION grest.tx_cbor IS 'Get raw transaction(s) in CBOR format'; -- noqa: LT01
-
-CREATE OR REPLACE FUNCTION grest.block_tx_cbor(_block_hashes text [])
-RETURNS TABLE (
-  tx_hash text,
-  block_hash text,
-  block_height word31type,
-  epoch_no word31type,
-  absolute_slot word63type,
-  tx_timestamp integer,
-  cbor text
-)
-LANGUAGE sql STABLE
-AS $$
-  SELECT *
-  FROM grest.tx_cbor(
-      (
-        SELECT ARRAY_AGG(ENCODE(hash, 'hex'))
-        FROM  tx
-        WHERE tx.block_id = ANY(
-          SELECT id
-          FROM block
-          WHERE hash = ANY(
-            SELECT DISTINCT DECODE(hashes_hex, 'hex')
-            FROM UNNEST(_block_hashes) AS hashes_hex
-          )
-        )
-      )
-    );
-$$;
-
-COMMENT ON FUNCTION grest.block_tx_cbor IS 'Get Raw transaction in CBOR format for given block hashes.'; -- noqa: LT01
+COMMENT ON FUNCTION grest.tx_by_metalabel IS 'Get all transactions that contain metadata with specified label(key)'; -- noqa: LT01
