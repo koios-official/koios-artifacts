9a97ff1fe5f27e606bac66edf63ec006d0e23b6a
Ola [AHLNET] <ola.ahlman@gmail.com>
2026-01-14 11:07:49 +0100

    asset_list Enhancement (#383)
    ## Description
    Fix optional filtering on policy and name to use indexed columns.
    PostgREST horizontal filtering cant do this on resulting encoded values.
    
    ## Which issue it fixes?
    Issue reported on Discord bugs channel from $papagoose
    
    ---------
    
    Co-authored-by: Dostrelith <12885604+dostrelith678@users.noreply.github.com>

--- files/grest/rpc/assets/asset_list.sql
+++ files/grest/rpc/assets/asset_list.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE FUNCTION grest.asset_list()
+CREATE OR REPLACE FUNCTION grest.asset_list(_asset_policy text DEFAULT NULL, _asset_name text DEFAULT NULL)
 RETURNS TABLE (
   policy_id text,
   asset_name text,
@@ -13,6 +13,8 @@ AS $$
     ENCODE(ma.name, 'escape')::text as asset_name_ascii,
     ma.fingerprint::text
   FROM public.multi_asset AS ma
+  WHERE CASE WHEN _asset_policy IS NULL THEN TRUE ELSE ma.policy = DECODE(_asset_policy, 'hex') END
+    AND CASE WHEN _asset_name IS NULL THEN TRUE ELSE ma.name = DECODE(_asset_name, 'hex') END
   ORDER BY ma.policy, ma.name;
 $$;
 
